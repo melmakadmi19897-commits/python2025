78dd236d7b3edcf2922f494261e282b811bb5f10
melmakadmi19897-commits <melmakadmi19897@iesjoanramis.org>
2025-11-13 11:23:48 +0100

    ex11

---
+++ prova1.py
@@ -0,0 +1,6 @@
+x = int(input("Escriure el primer nombre: "))
+y = int(input("Escriure el segon nombre: "))
+c = a + b
+print("El resultat de la suma {} + {} és {}".format(a, b, c))
+c = a * b 
+print("El resultat de la suma {} * {} és {}".format(a, b, c))
