0fe4d8e4bec68606af696734a51d554c8dc70fb9
melmakadmi19897-commits <melmakadmi19897@iesjoanramis.org>
2025-11-13 11:46:05 +0100

    ex12

---
+++ Ex12.py
@@ -0,0 +1,8 @@
+edat = int(input("Introdueix la teva edat: "))
+
+if edat > 18:
+    print("És major d'edat.")
+elif edat < 18:
+    print("No és major d'edat.")
+else:
+    print("Té exactament 18 anys.")
