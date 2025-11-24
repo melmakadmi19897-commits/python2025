e76c6c9ed849ded425c1b1d6c09edec73359fbaf
melmakadmi19897-commits <melmakadmi19897@iesjoanramis.org>
2025-11-24 09:44:03 +0100

    ex17

---
+++ Ex17.py
@@ -0,0 +1,12 @@
+def ex17(a):
+    return len(a)
+
+
+
+# Programa principal
+a=[2, 3, 4, "Pere", [3,4], "a", [1, [3,4],5]]
+print(ex17(a))
+b = "Som en Joan Ramis, i tú què tal estàs?"
+print(ex17(b))
+c=[]
+print(ex17(c))
